38029bea38c2a549d985bf8f52dc1516c6274205
Mark Rushakoff and Onsi Fakhouri <pair+mrushakoff+onsi@pivotallabs.com>
2013-08-01 17:21:57 -0700

    Now with more health_manager stats
    [#54338098]

--- lib/collector/handlers/health_manager.rb
+++ lib/collector/handlers/health_manager.rb
@@ -2,35 +2,32 @@
 module Collector
   class Handler
     class HealthManager < Handler
-      METRICS = {
-              "total" => {
-                      "apps" => "apps",
-                      "started_apps" => "started_apps",
-                      "instances" => "instances",
-                      "started_instances" => "started_instances",
-                      "memory" => "memory",
-                      "started_memory" => "started_memory",
-              },
-              "running" => {
-                      "apps" => "running_apps",
-                      "crashes" => "crashes",
-                      "running_instances" => "running_instances",
-                      "missing_instances" => "missing_instances",
-                      "flapping_instances" => "flapping_instances"
-              }
-      }
-
       def process(context)
         varz = context.varz
-        METRICS.each do |type, metric_map|
-          if type_varz = varz[type]
-            metric_map.each do |varz_name, metric_name|
-              if type_varz[varz_name]
-                send_metric("#{type}.#{metric_name}", type_varz[varz_name], context)
-              end
-            end
-          end
-        end
+        total_varz = varz["total"]
+        send_metric("total.apps", total_varz["apps"], context)
+        send_metric("total.started_apps", total_varz["started_apps"], context)
+        send_metric("total.instances", total_varz["instances"], context)
+        send_metric("total.started_instances", total_varz["started_instances"], context)
+        send_metric("total.memory", total_varz["memory"], context)
+        send_metric("total.started_memory", total_varz["started_memory"], context)
+
+        running_varz = varz["running"]
+        send_metric("running.crashes", running_varz["crashes"], context)
+        send_metric("running.running_apps", running_varz["apps"], context)
+        send_metric("running.running_instances", running_varz["running_instances"], context)
+        send_metric("running.missing_instances", running_varz["missing_instances"], context)
+        send_metric("running.flapping_instances", running_varz["flapping_instances"], context)
+
+        send_metric("time_to_analyze_all_droplets_in_seconds", varz["analysis_loop_duration"], context)
+        send_metric("time_to_retrieve_desired_state_in_seconds", varz["bulk_update_loop_duration"], context)
+
+        send_metric("total_heartbeat_messages_received", varz["heartbeat_msgs_received"], context)
+        send_metric("total_droplet_exited_messages_received", varz["droplet_exited_msgs_received"], context)
+        send_metric("total_droplet_update_messages_received", varz["droplet_updated_msgs_received"], context)
+        send_metric("total_hm_status_messages_received", varz["healthmanager_status_msgs_received"], context)
+        send_metric("total_hm_health_request_messages_received", varz["healthmanager_health_request_msgs_received"], context)
+        send_metric("total_hm_droplet_request_messages_received", varz["healthmanager_droplet_request_msgs_received"], context)
 
         total_users = varz["total_users"]
         return unless total_users
--- spec/unit/collector/handlers/health_manager_spec.rb
+++ spec/unit/collector/handlers/health_manager_spec.rb
@@ -13,16 +13,24 @@ describe "Collector::Handler::HealthManager" do
         "flapping_instances" => 11,
         "missing_instances" => 13,
         "running_instances" => 88,
-        "crashes" => 0,
+        "crashes" => 77,
         "apps" => 98
       },
       "total_users" => total_users,
+      "heartbeat_msgs_received" => 1716,
+      "droplet_exited_msgs_received" => 128,
+      "droplet_updated_msgs_received" => 68,
+      "healthmanager_status_msgs_received" => 451,
+      "healthmanager_health_request_msgs_received" => 35,
+      "healthmanager_droplet_request_msgs_received" => 18,
+      "analysis_loop_duration" => 60.5,
+      "bulk_update_loop_duration" => 1.25,
       "total" => {
         "started_memory" => 32000,
         "memory" => 176000,
         "started_instances" => 112,
         "instances" => 1193,
-        "started_apps" => 98,
+        "started_apps" => 2,
         "apps" => 150
       }
     }
@@ -31,12 +39,28 @@ describe "Collector::Handler::HealthManager" do
 
   it "sends metrics for every entry" do
     context = Collector::HandlerContext.new(nil, nil, varz)
-    handler.should_receive(:send_metric).with("running.crashes", 0, context)
-    handler.should_receive(:send_metric).with("running.running_apps", 98, context)
     handler.should_receive(:send_metric).with("total.apps", 150, context)
+    handler.should_receive(:send_metric).with("total.started_apps", 2, context)
+    handler.should_receive(:send_metric).with("total.instances", 1193, context)
+    handler.should_receive(:send_metric).with("total.started_instances", 112, context)
+    handler.should_receive(:send_metric).with("total.memory", 176000, context)
+    handler.should_receive(:send_metric).with("total.started_memory", 32000, context)
+    handler.should_receive(:send_metric).with("running.crashes", 77, context)
+    handler.should_receive(:send_metric).with("running.running_apps", 98, context)
+    handler.should_receive(:send_metric).with("running.running_instances", 88, context)
+    handler.should_receive(:send_metric).with("running.missing_instances", 13, context)
+    handler.should_receive(:send_metric).with("running.flapping_instances", 11, context)
     handler.should_receive(:send_metric).with("total_users", 687, context)
 
-    handler.should_receive(:send_metric).exactly(12 - 4).times
+    handler.should_receive(:send_metric).with("time_to_analyze_all_droplets_in_seconds", 60.5, context)
+    handler.should_receive(:send_metric).with("time_to_retrieve_desired_state_in_seconds", 1.25, context)
+
+    handler.should_receive(:send_metric).with("total_heartbeat_messages_received", 1716, context)
+    handler.should_receive(:send_metric).with("total_droplet_exited_messages_received", 128, context)
+    handler.should_receive(:send_metric).with("total_droplet_update_messages_received", 68, context)
+    handler.should_receive(:send_metric).with("total_hm_status_messages_received", 451, context)
+    handler.should_receive(:send_metric).with("total_hm_health_request_messages_received", 35, context)
+    handler.should_receive(:send_metric).with("total_hm_droplet_request_messages_received", 18, context)
 
     handler.process(context)
   end
@@ -49,12 +73,14 @@ describe "Collector::Handler::HealthManager" do
 
       handler.should_receive(:send_metric).twice.with("total_users", anything, anything)
       handler.should_receive(:send_metric).once.with("user_rate", 0.1, anything)
+      magic_number_with_magic_math = 20 * 2 - 2
+      handler.should_receive(:send_metric).exactly(magic_number_with_magic_math).times
 
-      varz = { "total_users" => 100 }
+      varz.merge!({ "total_users" => 100 })
       context = Collector::HandlerContext.new(nil, 0, varz)
       handler.process(context)
 
-      varz = { "total_users" => 100 + num_new_users }
+      varz.merge!({ "total_users" => 100 + num_new_users })
       context = Collector::HandlerContext.new(nil, time_diff, varz)
       handler.process(context)
 
@@ -62,7 +88,7 @@ describe "Collector::Handler::HealthManager" do
 
     context "when the collector has no prior information about the number of users (i.e., when it's started up)" do
       it "doesn't send a user rate metric" do
-        varz = { "total_users" => 123 }
+        varz.merge!({ "total_users" => 123 })
         context = Collector::HandlerContext.new(nil, nil, varz)
 
         handler.should_not_receive(:send_metric).with("user_rate", anything, context)
