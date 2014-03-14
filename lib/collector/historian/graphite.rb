cddedff8282fbd93d68abc4618bbc3beaa3cdf59
Simon Johansson <simon@simonjohansson.com>
2014-03-14 12:35:38 +0000

    Add the graphite historian.
    Signed-off-by: Johannes Tuchscherer <jtuchscherer@pivotallabs.com>

---
+++ lib/collector/historian/graphite.rb
@@ -0,0 +1,66 @@
+require 'collector/graphite_connection'
+
+module Collector
+  class Historian
+    class Graphite
+      attr_reader :connection
+      def initialize(host, port)
+        @host = host
+        @port = port
+        @connection = EventMachine.connect(@host, @port, GraphiteConnection)
+      end
+
+      def send_data(properties)
+        metrics_name = get_metrics_name(properties)
+        value = get_value(properties[:value])
+        timestamp = get_timestamp(properties[:timestamp])
+
+        if metrics_name && value && timestamp
+          @connection.send_data("#{metrics_name} #{value} #{timestamp}\n")
+        end
+      end
+
+      private
+
+      def get_metrics_name(p)
+        # Given a properties hash like so
+        # {:key=>"cpu_load_avg", :timestamp=>1394801347, :value=>0.25, :tags=>{:ip=>"172.30.5.74", :role=>"core", :job=>"CloudController", :index=>0, :name=>"CloudController/0", :deployment=>"CF"}}
+        # One will get a metrics key like so
+        # CF.CloudController0.cpu_load_avg
+        deployment = p[:tags][:deployment]
+        job = p[:tags][:job]
+        index = p[:tags][:index]
+        key = p[:key]
+
+        unless deployment && job && index && key
+          Config.logger.error("collector.create-graphite-key.fail: Could not create metrics name from fields tags.deployment, tags.job, tags.index or key.")
+          return nil
+        end
+
+        [deployment, job, index, key].join '.'
+      end
+
+      def get_value(value)
+        unless value.is_a? Numeric
+          Config.logger.error("collector.emit-graphite.fail: Value is not a float or int, got: #{value}")
+          return nil
+        end
+
+        value
+      end
+
+      def get_timestamp(ts)
+        if ts && is_epoch?(ts)
+          return ts
+        end
+
+        Time.now.to_i
+      end
+
+      def is_epoch?(ts)
+        /^1[0-9]{9}$/.match(ts.to_s)
+      end
+
+    end
+  end
+end
--- lib/collector/tsdb_connection.rb
+++ lib/collector/tsdb_connection.rb
@@ -1,29 +1,10 @@
+require_relative "tcp_connection"
+
 module Collector
   # TSDB connection for sending metrics
-  class TsdbConnection < EventMachine::Connection
-    def connection_completed
-      Config.logger.info("collector.tsdb-connected")
-      @port, @ip = Socket.unpack_sockaddr_in(get_peername)
-    end
-
-    def unbind
-      if @port && @ip
-        Config.logger.warn("collector.tsdb-connection-lost")
-        EM.add_timer(1.0) do
-          begin
-            reconnect(@ip, @port)
-          rescue EventMachine::ConnectionError => e
-            Config.logger.warn("collector.tsdb-connection-error", error: e, backtrace: e.backtrace)
-            unbind
-          end
-        end
-      else
-        Config.logger.fatal("collector.tsdb.could-not-connect")
-        exit!
-      end
-    end
-
-    def receive_data(_)
+  class TsdbConnection < Collector::TCPConnection
+    def initialize
+      super("collector.tsdb")
     end
   end
 end
