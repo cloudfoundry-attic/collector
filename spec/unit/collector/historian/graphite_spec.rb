38109df78b32723fab38db4e782c27c08b43007c
Simon Johansson <simon@simonjohansson.com>
2014-03-17 11:04:47 +0000

    The tests need a newline at the end.

--- spec/unit/collector/historian/graphite_spec.rb
+++ spec/unit/collector/historian/graphite_spec.rb
@@ -31,7 +31,7 @@ describe Collector::Historian::Graphite do
     it "converts the properties hash graphite data" do
       graphite_historian = described_class.new("host", 9999)
 
-      connection.should_receive(:send_data).with("CF.Blurgh.0.some_key 2 1234568912")
+      connection.should_receive(:send_data).with("CF.Blurgh.0.some_key 2 1234568912\n")
       graphite_historian.send_data(metric_payload)
     end
 
@@ -40,7 +40,7 @@ describe Collector::Historian::Graphite do
         graphite_historian = described_class.new("host", 9999)
         metric_payload.delete(:timestamp)
         Timecop.freeze Time.now.to_i do
-          connection.should_receive(:send_data).with("CF.Blurgh.0.some_key 2 #{Time.now.to_i}")
+          connection.should_receive(:send_data).with("CF.Blurgh.0.some_key 2 #{Time.now.to_i}\n")
           graphite_historian.send_data(metric_payload)
         end
       end
@@ -52,7 +52,7 @@ describe Collector::Historian::Graphite do
 
         metric_payload.update(:timestamp => "BLURGh!!11")
         Timecop.freeze Time.now.to_i do
-          connection.should_receive(:send_data).with("CF.Blurgh.0.some_key 2 #{Time.now.to_i}")
+          connection.should_receive(:send_data).with("CF.Blurgh.0.some_key 2 #{Time.now.to_i}\n")
           graphite_historian.send_data(metric_payload)
         end
       end
